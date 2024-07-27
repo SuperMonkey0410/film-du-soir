628d0879edba923a6978018d5b8529645109bda7
SuperMonkey0410 <145975354+SuperMonkey0410@users.noreply.github.com>
2024-07-27 16:38:20 +0300

    Добавил view & forms для логина

--- drfsite/users/views.py
+++ drfsite/users/views.py
@@ -1,12 +1,29 @@
-from django.shortcuts import render
+from django.shortcuts import render, HttpResponseRedirect
+from django.urls import reverse
+
+from .models import *
+from .forms import UserLoginForm
+from django.contrib import auth
 
 
 def register(request, ):
-    return render(request, 'users/register.html')
+    return render(request, 'users/register.html', )
 
 
 def login(request, ):
-    return render(request, 'users/login.html')
+    form = UserLoginForm(data=request.POST)
+    if form.is_valid():
+        username = request.POST['username']
+        password = request.POST['password']
+        user = auth.authenticate(username=username, password=password)
+        if user:
+            auth.login(request, user)
+            return HttpResponseRedirect(reverse('cinema:home'))
+    else:
+        form = UserLoginForm()
+
+    context = {'form': form}
+    return render(request, 'users/login.html', context=context)
 
 
 def logout(request, ):
