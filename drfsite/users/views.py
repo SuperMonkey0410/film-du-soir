f7d79c52098b4dd01e3a3b96218c581ba2e14f9a
SuperMonkey0410 <145975354+SuperMonkey0410@users.noreply.github.com>
2024-07-27 18:55:04 +0300

    добавил view register, logout

--- drfsite/users/views.py
+++ drfsite/users/views.py
@@ -1,24 +1,38 @@
-from django.shortcuts import render, HttpResponseRedirect
+from django.shortcuts import render, HttpResponseRedirect, redirect
 from django.urls import reverse
 
 from .models import *
-from .forms import UserLoginForm
+from .forms import UserLoginForm, RegistrationForm
 from django.contrib import auth
 
 
 def register(request, ):
-    return render(request, 'users/register.html', )
-
-
-def login(request, ):
-    form = UserLoginForm(data=request.POST)
-    if form.is_valid():
-        username = request.POST['username']
-        password = request.POST['password']
-        user = auth.authenticate(username=username, password=password)
-        if user:
+    if request.method == 'POST':
+        form = RegistrationForm(data=request.POST)
+        if form.is_valid():
+            form.save()
+            user = form.instance
             auth.login(request, user)
-            return HttpResponseRedirect(reverse('cinema:home'))
+            return HttpResponseRedirect(reverse('users:home'))
+
+    else:
+        form = RegistrationForm()
+    context = {'form': form}
+
+    return render(request, 'users/register.html', context=context)
+
+
+def login(request):
+    if request.method == 'POST':
+        form = UserLoginForm(data=request.POST)
+        if form.is_valid():
+            username = request.POST['username']
+            password = request.POST['password']
+            user = auth.authenticate(username=username, password=password)
+            if user:
+                auth.login(request, user)
+
+                return HttpResponseRedirect(reverse('cinema:home'))
     else:
         form = UserLoginForm()
 
@@ -27,7 +41,8 @@ def login(request, ):
 
 
 def logout(request, ):
-    pass
+    auth.logout(request)
+    return redirect(reverse('cinema:home'))
 
 
 def profile(request, ):
